940f391a470bb54b2ded7f860f3af789f045ed4c
Hana Borová <han.krpenska@centrum.cz>
2024-05-22 09:29:26 +0200

    correction

--- Ot4_rozdil_rustu_cen_a_mezd.sql
+++ Ot4_rozdil_rustu_cen_a_mezd.sql
@@ -1,33 +1,38 @@
 WITH price_growth_total AS
 (
 	SELECT
+		hbf.food_name,
 		hbf.current_year,
-		round(((hbf2.price - hbf.price)/hbf.price) *100, 2) AS price_growth_percent
+		round(((hbf.price - hbf2.price)/hbf2.price) *100, 2) AS price_growth_percent
 	FROM t_hana_borova_project_sql_primary_final hbf
 	JOIN t_hana_borova_project_sql_primary_final hbf2
 		ON hbf.food_name = hbf2.food_name 
 		AND hbf.current_year = hbf2.current_year + 1
-	GROUP BY hbf.current_year
+	GROUP BY hbf.food_name, hbf.current_year
 ),
 wage_growth_total AS 
 (
 	SELECT
+		hbf.category_name,
 		hbf.current_year,
-		round(((hbf2.avg_wage_per_year - hbf.avg_wage_per_year)/hbf.avg_wage_per_year)* 100, 2) AS wage_growth_percent
+		round(((hbf.avg_wage_per_year - hbf2.avg_wage_per_year)/hbf2.avg_wage_per_year)* 100, 2) AS wage_growth_percent
 	FROM t_hana_borova_project_sql_primary_final hbf
 	JOIN t_hana_borova_project_sql_primary_final hbf2
 		ON hbf.category_name = hbf2.category_name 
 		AND hbf.current_year = hbf2.current_year + 1
-	GROUP BY hbf.current_year
+	GROUP BY hbf.category_name, hbf.current_year
 )
 SELECT 
 	pgt.current_year,
-	pgt.price_growth_percent,
-	wgt.wage_growth_percent,
+	round(avg(pgt.price_growth_percent),2) AS price_growth_perc,
+	round(avg(wgt.wage_growth_percent),2) AS wage_growth_perc,
+	round(avg(pgt.price_growth_percent) - avg(wgt.wage_growth_percent),2) AS difference,
 	CASE 
-		WHEN (pgt.price_growth_percent - wgt.wage_growth_percent) >=10 THEN 'higher_then_10%'
+		WHEN (avg(pgt.price_growth_percent) - avg(wgt.wage_growth_percent)) >=10 THEN 'higher_then_10%'
 		ELSE 'lower_then_10%'
 	END AS price_wage_diff
 FROM price_growth_total pgt
 LEFT JOIN wage_growth_total wgt
-	ON pgt.current_year = wgt.current_year;
+	ON pgt.current_year = wgt.current_year
+GROUP BY pgt.current_year;
+;
--- ot5_vliv_HDP_na_ceny_mzdy.sql
+++ ot5_vliv_HDP_na_ceny_mzdy.sql
@@ -1,30 +1,32 @@
 WITH price_growth_total AS
 (
 	SELECT
+		hbf.food_name,
 		hbf.current_year,
-		round(((hbf2.price - hbf.price)/hbf.price)*100, 2) AS price_growth_percent
+		round(avg(((hbf.price - hbf2.price)/hbf2.price)*100), 2) AS price_growth_percent
 	FROM t_hana_borova_project_sql_primary_final hbf
 	JOIN t_hana_borova_project_sql_primary_final hbf2
 		ON hbf.food_name = hbf2.food_name 
 		AND hbf.current_year = hbf2.current_year + 1
-	GROUP BY hbf.current_year
-),
+	GROUP BY hbf.food_name, hbf.current_year
+), 
 wage_growth_total AS 
 (	
 	SELECT
+		hbf.category_name,
 		hbf.current_year,
-		round(((hbf2.avg_wage_per_year - hbf.avg_wage_per_year)/hbf.avg_wage_per_year) * 100, 2) AS wage_growth_percent
+		round(avg(((hbf.avg_wage_per_year - hbf2.avg_wage_per_year)/hbf2.avg_wage_per_year) * 100), 2) AS wage_growth_percent
 	FROM t_hana_borova_project_sql_primary_final hbf
 	JOIN t_hana_borova_project_sql_primary_final hbf2
 		ON hbf.category_name = hbf2.category_name 
 		AND hbf.current_year = hbf2.current_year + 1
-	GROUP BY hbf.current_year
+	GROUP BY hbf.category_name, hbf.current_year
 ),
 GDP_growth_total AS
 (
 	SELECT
 		hbf.current_year,
-		round(((hbf2.GDP - hbf.GDP)/hbf.GDP) * 100, 2) AS GDP_growth_percent
+		round(avg(((hbf.GDP - hbf2.GDP)/hbf2.GDP) * 100), 2) AS GDP_growth_percent
 	FROM t_hana_borova_project_sql_primary_final hbf
 	JOIN t_hana_borova_project_sql_primary_final hbf2
 		ON hbf.category_name = hbf2.category_name 
@@ -33,11 +35,13 @@ GDP_growth_total AS
 )
 SELECT 
 	pgt.current_year,
-	pgt.price_growth_percent,
-	wgt.wage_growth_percent,
-	ggt.GDP_growth_percent
+	round(avg(pgt.price_growth_percent), 2) AS price_growth_perc,
+	round(avg(wgt.wage_growth_percent),2) AS wage_growth_perc,
+	round(avg(ggt.GDP_growth_percent),2) AS GDP_growth_perc
 FROM price_growth_total pgt
 LEFT JOIN wage_growth_total wgt
 	ON pgt.current_year = wgt.current_year
 LEFT JOIN GDP_growth_total ggt
-	ON ggt.current_year = pgt.current_year;
+	ON ggt.current_year = pgt.current_year
+GROUP BY pgt.current_year
+;
